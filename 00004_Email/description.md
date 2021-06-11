f7d7f4d65b0b25c600d304118e148fffe0584a69
mayra@mumuki.org <mayra@mumuki.org>
2021-06-11 16:59:55 +0000

    Mumuki Export on 2021-06-11 16:59:55 +0000

--- 00004_Email/description.md
+++ 00004_Email/description.md
@@ -1,13 +1,14 @@
-- Partiendo de un objeto guardado en la variable `user`
+- Partiendo de un objeto guardado en la variable `perfil`
 - Verificar si tiene la propiedad `email`
   - Si no la tiene, mostrar en pantalla: 'El usuario no tiene la propiedad email'
   - Si la tiene, mostrar en pantalla: 'El usuario tiene la propiedad email'
 
 ```js
-const user = {
-  username: "ada_lovelace",
-  password: "1234567890!",
+let perfil = {
+  nombreDeCuenta: "ada_lovelace",
+  contrasenia: "1234567890!",
 };
 
-// acá tu solucion
 ```
+
+> Definí la función `tieneEmail` que reciba un `perfil` por parámetro y retorne si el `perfil` tiene un `email`
--- 00004_Email/test..js
+++ 00004_Email/test..js
@@ -1,5 +1,22 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
+describe("", function() {
+  it("Si el perfil tiene un email, la función tieneEmail retorna true", function() {
+    let perfil = {
+      nombreDeCuenta: "ada_lovelace",
+      contrasenia: "1234567890!",
+      email: 'ada_lovelace@gmail.com'
+    };
+
+    assert(tieneEmail(perfil))
+  })
+})
+
+describe("", function() {
+  it("Si el perfil no tiene un email, la función tieneEmail retorna false", function() {
+    let perfil = {
+      nombreDeCuenta: "ada_lovelace",
+      contrasenia: "1234567890!",
+    };
+
+    assert.equal(tieneEmail(perfil), false)
   })
 })
