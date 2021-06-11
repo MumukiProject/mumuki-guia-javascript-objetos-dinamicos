0bd21af7d1bdc3fb216db10895d02b2b0f809069
mayra@mumuki.org <mayra@mumuki.org>
2021-06-11 17:30:32 +0000

    Mumuki Export on 2021-06-11 17:30:32 +0000

--- 00003_Producto/test..js
+++ 00003_Producto/test..js
@@ -1,1 +1,16 @@
-TODO
+describe("", function() {
+  it("Si tenemos el siguiente producto ", function() {
+    let let producto = {
+      id: "ADA-020",
+      titulo: "Gubergren sed est amet voluptua",
+      precio: 123.75,
+      imagen:
+        "https://i.kinja-img.com/gawker-media/image/upload/s--53mPze4a--/c_scale,f_auto,fl_progressive,q_80,w_800/1315358249455433031.jpg",
+      condicion: "nuevo",
+      envioGratis: true,
+      ubicacion: "Capital Federal",
+    };
+    let resultado = "producto[id] -> ADA-020\nproducto[titulo] -> Gubergren sed est amet voluptua\nproducto[precio] -> 123.75\nproducto[imagen] -> https://i.kinja-img.com/gawker-media/image/upload/s--53mPze4a--/c_scale,f_auto,fl_progressive,q_80,w_800/1315358249455433031.jpg\nproducto[condicion] -> nuevo\nproducto[envioGratis] -> true\nproducto[ubicacion] -> Capital Federal\n"
+    assert.equal(obtenerPropiedadesYValores(producto), resultado)
+  })
+})
