354e63284567b223088329756228bb1a0734e00a
mayra@mumuki.org <mayra@mumuki.org>
2021-06-11 17:01:49 +0000

    Mumuki Export on 2021-06-11 17:01:49 +0000

--- 00004_Email/test..js
+++ 00004_Email/test..js
@@ -1,22 +1,22 @@
 describe("", function() {
-  it("Si el perfil tiene un email, la función tieneEmail retorna true", function() {
+  it("Si el perfil tiene un email, la función tieneEmail retorna El usuario tiene la propiedad email", function() {
     let perfil = {
       nombreDeCuenta: "ada_lovelace",
       contrasenia: "1234567890!",
       email: 'ada_lovelace@gmail.com'
     };
 
-    assert(tieneEmail(perfil))
+    assert.equal(tieneEmail(perfil), 'El usuario tiene la propiedad email')
   })
 })
 
 describe("", function() {
-  it("Si el perfil no tiene un email, la función tieneEmail retorna false", function() {
+  it("Si el perfil no tiene un email, la función tieneEmail retorna El usuario no tiene la propiedad email", function() {
     let perfil = {
       nombreDeCuenta: "ada_lovelace",
       contrasenia: "1234567890!",
     };
 
-    assert.equal(tieneEmail(perfil), false)
+    assert.equal(tieneEmail(perfil), 'El usuario no tiene la propiedad email')
   })
 })
